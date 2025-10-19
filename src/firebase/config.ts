81ca60d662d779d00dd30c2a37e909f1d3e575ad
xazratbek turdaliyev <xazratbek027@gmail.com>
2025-10-19 05:16:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3441452454-c82e9",
+  "appId": "1:243140839363:web:4241bf4ddbcafa95c29f76",
+  "apiKey": "AIzaSyDP53vBa2Z1pVOnhvMXy_m_T3WIUu3SszU",
+  "authDomain": "studio-3441452454-c82e9.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "243140839363"
+};
