45428e5d281098acdc50aaacd41b834955705927
xazratbek turdaliyev <xazratbek027@gmail.com>
2025-10-19 07:56:30 +0000

    Error: `DialogContent` requires a `DialogTitle` for the component to be

--- src/components/lightbox.tsx
+++ src/components/lightbox.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { Dialog, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogDescription, DialogTitle } from "@/components/ui/dialog";
 import Image from "next/image";
 
 interface LightboxProps {
@@ -14,11 +14,13 @@ export default function Lightbox({ imageUrl, open, onOpenChange }: LightboxProps
   return (
     <Dialog open={open} onOpenChange={onOpenChange}>
       <DialogContent className="max-w-[90vw] max-h-[90vh] w-auto h-auto p-0 bg-transparent border-0 flex items-center justify-center">
+        <DialogTitle className="sr-only">Kattalashtirilgan rasm</DialogTitle>
+        <DialogDescription className="sr-only">Loyiha rasmining to'liq ekranli ko'rinishi.</DialogDescription>
         <div className="relative w-[90vw] h-[90vh]">
           <Image 
             src={imageUrl} 
             alt="Kattalashtirilgan rasm" 
-            layout="fill"
+            fill
             objectFit="contain"
             className="rounded-lg"
           />
